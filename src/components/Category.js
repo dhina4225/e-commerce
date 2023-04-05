c035c5992666fca8d45a81e014f88fde5509e70c
Dina <Dinakaran0651@gmail.com>
2023-04-05 08:58:13 +0530

    update all files

---
+++ src/components/Category.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Category = () => {
+  fetch;
+  return <div></div>;
+};
+
+export default Category;
---
+++ src/fireabase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0dLREoPO2aIQu74NESbta_dAw6givs_w",
+  authDomain: "e-commerce-38a40.firebaseapp.com",
+  projectId: "e-commerce-38a40",
+  storageBucket: "e-commerce-38a40.appspot.com",
+  messagingSenderId: "192253936982",
+  appId: "1:192253936982:web:88c3e439e3b16a47ebc2f7",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
